3c2dc2ee19ac14735022bedc735282f37ba0c1f5
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-21 23:26:41 -0300

    Gerar limites do mapa

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 class Boundary {
+    static width = 40;
+    static height = 40;
     constructor({ position }){
         this.position = position;
         this.width = 40;
@@ -18,7 +20,7 @@ class Boundary {
 }
 
 
-const map = [
+/*const map = [
     ['1', '-', '-', '-', '-', '-', '-', '-', '-', '-', '2'],
     ['|', '.', '.', '.', '.', '.', '.', '.', '.', '.', '|'],
     ['|', '.', 'b', '.', '[', '7', ']', '.', 'b', '.', '|'],
@@ -32,7 +34,16 @@ const map = [
     ['|', '.', 'b', '.', '[', '5', ']', '.', 'b', '.', '|'],
     ['|', '.', '.', '.', '.', '.', '.', '.', '.', 'p', '|'],
     ['4', '-', '-', '-', '-', '-', '-', '-', '-', '-', '3']
-  ]
+]*/
+
+
+const map = [
+    ['-', '-', '-', '-', '-', '-'],
+    ['-', ' ', ' ', ' ', ' ', '-'],
+    ['-', ' ', '-', '-', ' ', '-'],
+    ['-', ' ', ' ', ' ', ' ', '-'],
+    ['-', '-', '-', '-', '-', '-'],
+]  
 
 const boundaries = [];
 
@@ -43,8 +54,8 @@ map.forEach((row, i) => {
             boundaries.push(
                 new Boundary({
                     position: {
-                        x: 40 * j,
-                        y: 40 * i
+                        x: Boundary.width * j,
+                        y: Boundary.height * i
                     }
                 })
             )
