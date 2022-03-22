c0a9a4c52c1d5c183d69d5f47bb3cadbcde9c718
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-22 11:15:30 -0300

    Update colisao

--- js/script.js
+++ js/script.js
@@ -123,7 +123,7 @@ function animate(){
         boundary.draw();
         
         //detecção de colisão
-        if(player.position.y - player.radius <= boundary.position.y + boundary.height && player.position.x + player.radius >= boundary.position.x && player.position.y + player.radius >= boundary.position.y && player.position.x - player.radius <= boundary.position.x + boundary.width){
+        if(player.position.y - player.radius + player.velocity.y <= boundary.position.y + boundary.height && player.position.x + player.radius + player.velocity.x >= boundary.position.x && player.position.y + player.radius + player.velocity.y >= boundary.position.y && player.position.x - player.radius + player.velocity.x <= boundary.position.x + boundary.width){
             console.log('estamos colidindo');
             player.velocity.x = 0;
             player.velocity.y = 0;
